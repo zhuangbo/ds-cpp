9d0f2a35be1bf249421d7d060b0562c167476736
Bo Zhuang <sdzhuangbo@126.com>
2017-08-25 21:54:10 +0800

    Add example code - triplet

---
+++ src/ch01/triplet.h
@@ -0,0 +1,66 @@
+///////////////////////////////////////
+/// file: triplet.h
+/// 三元组数据类型
+///////////////////////////////////////
+
+#pragma once
+
+#include <exception> // for std::out_of_range
+
+///////////////////////////////////////
+/// 存储结构定义
+///
+template <typename T>
+struct Triplet {
+    T elem[3];
+};
+
+///////////////////////////////////////
+/// 基本操作
+///
+
+///
+/// 返回三元组 t 的第 i 个元素（1<=i<=3）。
+///
+template <typename T>
+const T& Get(const Triplet<T>& t, int i)
+{
+    if(i<1 || i>3)
+        throw std::out_of_range("i<1 or i>3");
+    
+    return t.elem[i-1];
+}
+
+///
+/// 改变三元组 t 的第 i 个元素（1<=i<=3）的值为 e。
+///
+template <typename T>
+void Put(Triplet<T>& t, int i, const T& e)
+{
+    if(i<1 || i>3)
+        throw std::out_of_range("i<1 or i>3");
+    
+    t.elem[i-1] = e;
+}
+
+// ///////////////////////////////////////
+// /// 对比以下简化版本，理解引用型参数的作用
+// ///
+
+// // 不足之处：与上述常引用版本相比，在参数传递和返回结果时
+// // 都会产生额外的存储分配和释放，可能导致低效率。
+// // 没有检查参数合法性，缺少错误处理。
+// template <typename T>
+// T Get(Triplet<T> t, int i)
+// {
+//     return t.elem[i-1];
+// }
+
+// // 不足之处：与上述常引用版本相比，在参数传递时会产生
+// // 额外的存储分配和释放，可能导致低效率。
+// // 没有检查参数合法性，缺少错误处理。
+// template <typename T>
+// void Put(Triplet<T>& t, int i, T e)
+// {
+//     t.elem[i-1] = e;
+// }
---
+++ src/ch01/triplet_test.cpp
@@ -0,0 +1,46 @@
+///////////////////////////////////////
+/// file: triplet_test.cpp
+/// Triplet tests
+///////////////////////////////////////
+
+#include <iostream>
+#include "triplet.h" // for Triplet
+
+///
+/// 重载输出运算符 << 输出三元组
+///
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Triplet<T>& t)
+{
+    out << '(' << t.elem[0] << ','
+        << t.elem[1] << ','
+        << t.elem[2] << ')';
+    return out;
+}
+
+
+///
+/// 测试三元组
+///
+int main()
+{
+    Triplet<int> t = {1,2,3};
+    std::cout << Get(t,1) << Get(t,2) << Get(t,3) << std::endl;
+
+    try {
+        Put(t,2,5);
+        Put(t,3,6);
+        Put(t,4,7);  // error
+    } catch(std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    std::cout << t << std::endl;
+    
+    Triplet<char> c = {'a','b','c'};
+    std::cout << c << std::endl;
+    
+    Triplet<double> d = {3.14159, 2.71828, 0.61803};
+    std::cout << d << std::endl;
+    
+    return 0;
+}
