11403124b67616e4f9eb3989894a9c67643ab30b
Bo Zhuang <sdzhuangbo@126.com>
2017-09-06 01:04:15 +0800

    Add test for locate and traverse of linklist

---
+++ src/ch02/linklist_test2.cpp
@@ -0,0 +1,64 @@
+///////////////////////////////////////
+/// file: linklist_test.cpp
+///////////////////////////////////////
+
+#include <iostream>
+#include "linklist.h"
+using namespace std;
+
+///
+/// 打印链表
+///
+template<typename T>
+void PrintList(LinkList<T> L)
+{
+    cout << "(";
+    auto p = L->next;
+    while(p) {
+        cout << " " << p->data;
+        p = p->next;
+    }
+    cout << " )" << endl;
+}
+
+bool eq(int a, int b)
+{
+    return a==b;
+}
+
+void print(int x)
+{
+    cout << " " << x;
+}
+
+int main()
+{
+    // 建立链表
+    LinkList<int> L;
+    // 初始化
+    InitList(L);
+    PrintList(L);
+
+    // 插入元素
+    ListInsert(L,1,3);
+    ListInsert(L,2,1);
+    ListInsert(L,3,4);
+    ListInsert(L,4,1);
+    ListInsert(L,5,5);
+    ListInsert(L,6,9);
+    PrintList(L);
+
+    // 元素定位
+    cout << "LocateElem 9 = ";
+    cout << "L(@" << LocateElem(L,9,eq) << ")" << endl;
+
+    // 遍历线性表
+    cout << "ListTraverse: ";
+    ListTraverse(L,print);
+    cout << endl;
+
+    // 销毁链表
+    DestroyList(L);
+
+    return 0;
+}
