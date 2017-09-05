85992a108d600c6126072c1aaa48de67fd0deaef
Bo Zhuang <sdzhuangbo@126.com>
2017-09-06 01:03:48 +0800

    Add linklist

---
+++ src/ch02/linklist.h
@@ -0,0 +1,210 @@
+///////////////////////////////////////
+/// file: linklist.h
+///////////////////////////////////////
+
+#pragma once
+
+#include <stdexcept>
+using std::out_of_range;
+
+///////////////////////////////////////
+/// 存储结构
+
+///
+/// 单链表存储结构
+///
+template<typename T>
+struct LNode
+{
+    T data;
+    LNode *next;
+};
+
+/// 指针作为单链表类型
+template<typename T>
+using LinkList = LNode<T> *; // C++11 only
+// 需要 C++11 编译（如：g++ -std=c++11 ...）
+
+///////////////////////////////////////
+/// 基本操作
+
+///
+/// 初始化带头结点的空链表
+///
+template<typename T>
+void InitList(LinkList<T>& L)
+{
+    // 创建头结点
+    L = new LNode<T>();
+    // 初始化为空表
+    L->next = NULL;
+}
+
+///
+/// 清空链表
+///
+template<typename T>
+void ClearList(LinkList<T>& L)
+{
+    // 当表不空时，逐个删除结点
+    while(L->next) {
+        LinkList<T> p = L->next;  // p 指向第一个数据结点
+        L->next = p->next;  // 从链表中解除结点
+        delete p;  // 释放结点存储空间
+    }
+}
+
+///
+/// 销毁链表
+///
+template<typename T>
+void DestroyList(LinkList<T>& L)
+{
+    ClearList(L);  // 清空链表
+    delete L;  // 释放头结点
+    L = NULL;  // 指针置空（防野指针）
+}
+
+///
+/// 判断链表是否是空表
+///
+template<typename T>
+bool ListEmpty(LinkList<T> L)
+{
+    return L->next==NULL;
+}
+
+///
+/// 求表长
+///
+template<typename T>
+int ListLength(LinkList<T> L)
+{
+    int n = 0;
+    auto p = L;  // for C++11 or LinkList<T> p = L;
+    while(p->next) {
+        ++n;
+        p = p->next;
+    }
+    return n;
+}
+
+///
+/// 取元素
+///
+template<typename T>
+void GetElem(LinkList<T> L, int i, T& e)
+{
+    // 找到第 i 个结点 p
+    int j = 1;
+    auto p = L->next;
+    while(p && j<i) {
+        p = p->next;
+        ++j;
+    }
+    // 若存在，则取数据，否则抛出异常
+    if(p && j==i) {  // 找到第 i 个结点
+        e = p->data;  // 取出结点数据
+    } else {  // 不存在第 i 个结点
+        throw std::out_of_range("i out of range");
+    }
+}
+
+///
+/// 取元素
+///
+template<typename T>
+T GetElem(LinkList<T> L, int i)
+{
+    // 找到第 i 个结点 p
+    int j = 1;
+    auto p = L->next;
+    while(p && j<i) {
+        p = p->next;
+        ++j;
+    }
+    // 若存在，则返回数据，否则抛出异常
+    if(p && j==i) {  // 找到第 i 个结点
+        return p->data;  // 返回结点数据
+    } else {  // 不存在第 i 个结点
+        throw std::out_of_range("i out of range");
+    }
+}
+
+///
+/// 插入数据
+///
+template<typename T>
+void ListInsert(LinkList<T>& L, int i, T e)
+{
+    // 找到第 i-1 个结点 p
+    int j = 0;
+    auto p = L;
+    while(p && j<i-1) {
+        p = p->next;
+        ++j;
+    }
+    // 若找到，则在 p 后插入 e，否则抛出异常
+    if(p && j==i-1) {
+        // 新建结点
+        auto s = new LNode<T>();
+        s->data = e;
+        // 在链表中插入结点
+        s->next = p->next;
+        p->next = s;
+    } else {
+        throw out_of_range("i out of range");
+    }
+}
+
+///
+/// 删除数据
+///
+template<typename T>
+void ListDelete(LinkList<T>& L, int i, T& e)
+{
+    // 寻找找第 i-1 个结点 p
+    int j = 0;
+    auto p = L;
+    while(p && j<i-1) {
+        p = p->next;
+        ++j;
+    }
+    // 若找到且存在第 i 个结点，则删除 p 的下一个结点，否则抛出异常
+    if(p && p->next && j==i-1) {
+        auto q = p->next;   // q 指向第 i 个结点
+        p->next = q->next;  // 从链表中移除结点 q
+        e = q->data;        // 取被删除结点的数据
+        delete q;           // 释放结点的存储空间
+    } else {
+        throw out_of_range("i out of range");
+    }
+}
+
+
+///
+/// 返回单链表 L 中第一个与 e 满足关系 compare 的数据结点的指针
+/// 若这样的数据元素不存在，则返回空指针 NULL。
+///
+template<typename T, typename Cmp>
+LinkList<T> LocateElem(LinkList<T> L, const T& e, Cmp compare)
+{
+    // 逐个取出元素与 e 比较
+    for(auto p = L->next; p; p = p->next) {
+        // 若满足条件，则返回指针
+        if(compare(p->data,e))  return p;
+    }
+
+    return NULL; // 不存在
+}
+
+///
+/// 遍历单链表，依次对 L 中的每个数据元素调用函数 visit
+///
+template<typename T, typename Func>
+void ListTraverse(LinkList<T> L, Func visit)
+{
+    for(auto p = L->next; p; p = p->next) {
+        visit(p->data);
+    }
+}
---
+++ src/ch02/linklist_test.cpp
@@ -0,0 +1,61 @@
+///////////////////////////////////////
+/// file: linklist_test.cpp
+///////////////////////////////////////
+
+#include <iostream>
+#include "linklist.h"
+using namespace std;
+
+///
+/// 打印链表
+///
+template<typename T>
+void PrintList(LinkList<T> L)
+{
+    cout << "(";
+    auto p = L->next;
+    while(p) {
+        cout << " " << p->data;
+        p = p->next;
+    }
+    cout << " )" << endl;
+}
+
+int main()
+{
+    // 建立链表
+    LinkList<int> L;
+    // 初始化
+    InitList(L);
+    PrintList(L);
+
+    // 插入元素
+    ListInsert(L,1,3);
+    ListInsert(L,2,1);
+    ListInsert(L,3,4);
+    ListInsert(L,4,1);
+    ListInsert(L,5,5);
+    ListInsert(L,6,9);
+    PrintList(L);
+
+    try {
+        ListInsert(L,8,0);
+    }
+    catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+    
+    // 取元素
+    cout << "The 1st element is " << GetElem(L,1) << endl;
+
+    // 删除元素
+    int e;
+    ListDelete(L,3,e);
+    PrintList(L);
+    cout << "Element deleted " << e << endl;
+
+    // 销毁链表
+    DestroyList(L);
+
+    return 0;
+}
