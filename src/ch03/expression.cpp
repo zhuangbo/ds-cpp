8620dbad0a373aa8cfc191718abaf674c39bbfb9
Bo Zhuang <sdzhuangbo@126.com>
2017-09-12 08:02:05 +0800

    Change function Read

--- src/ch03/expression.cpp
+++ src/ch03/expression.cpp
@@ -44,16 +44,20 @@ int Operate(int a, char theta, int b)
     throw "Unknown operator";
 }
 
-/// 读取运算符或操作数，返回读取的第一个字符
-/// 若为运算符，isop 为 true 且 op 返回运算符
-/// 否则，num 返回操作数
-char Read(bool& isop, char& op, int& num)
+/// 读取运算符或操作数，分别返回 OPERATOR 或 OPERAND
+/// op 返回运算符，num 返回操作数
+enum {OPERATOR,OPERAND};
+int Read(char& op, int& num)
 {
     char c = cin.peek();  // 查看输入流中的下一个字符
     while(isspace(c)) { cin.ignore(1); c = cin.peek(); } // 跳过空白
-    isop = c<'0' || c>'9';  // 非数字即为运算符
-    isop ? (cin >> op) : (cin >> num); // 读取运算符或操作数
-    return c;
+    if(c>='0' && c<='9') {  // 数字开头即为操作数
+        cin >> num; // 读取操作数
+        return OPERAND;
+    } else {  // 非数字即为运算符
+        cin >> op;  // 读取运算符
+        return OPERATOR;
+    }
 }
 
 ///
@@ -62,7 +66,7 @@ char Read(bool& isop, char& op, int& num)
 ///
 int EvaluateExpression() {
     char c, x;
-    bool isop;  // 当前输入是运算符时为 true
+    int isop;   // 当前输入是运算符（OPERATOR）或操作数（OPERAND）
     char op;    // 当前输入的运算符
     int num;    // 当前输入的操作数
     SqStack<char> optr;  // 运算符栈
@@ -74,23 +78,23 @@ int EvaluateExpression() {
     InitStack(opnd);
     // 表达式开始
     Push(optr, '#');
-    // 读取一个运算符 op 或操作数 num，读取操作数时 isop 为真
-    c = Read(isop, op, num); // c 为第一个字符
+    // 读取一个运算符 op 或操作数 num
+    isop = Read(op, num);
     // 当表达式未结束或还有未计算的运算符时继续
-    while(c!='#' || GetTop(optr)!='#') {
+    while(isop==OPERAND || op!='#' || GetTop(optr)!='#') {
         // 处理运算符或操作数
-        if(! isop) { // 遇到操作数，直接入栈，继续读取
+        if(isop==OPERAND) { // 遇到操作数，直接入栈，继续读取
             Push(opnd, num);
-            c = Read(isop, op, num);
+            isop = Read(op, num);
         } else {  // 遇到运算符，根据优先级分别处理
             switch(Precede(GetTop(optr), op)) {
             case '<':  // 栈顶运算符优先级低，运算符入栈，继续读取
                 Push(optr, op);
-                c = Read(isop, op, num);
+                isop = Read(op, num);
                 break;
             case '=':  // 脱括号，继续读取
                 Pop(optr, x);
-                c = Read(isop, op, num);
+                isop = Read(op, num);
                 break;
             case '>':  // 栈顶运算符优先级高，执行运算，结果入栈
                 Pop(optr, theta);
