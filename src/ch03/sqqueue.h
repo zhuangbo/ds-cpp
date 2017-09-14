553ea8735f5f023014e23ecadc50c93317e506c4
Bo Zhuang <sdzhuangbo@126.com>
2017-09-14 23:39:11 +0800

    Add SqQueue and test

---
+++ src/ch03/sqqueue.h
@@ -0,0 +1,94 @@
+///////////////////////////////////////
+/// file: sqqueue.h
+/// 循环队列
+///////////////////////////////////////
+
+#pragma once
+
+#include <stdexcept>
+using std::overflow_error;
+using std::underflow_error;
+
+///////////////////////////////////////
+/// 存储结构
+
+/// 循环队列
+template<typename T, int MAXSIZE=256>
+struct SqQueue
+{
+    T elem[MAXSIZE];    // 数据元素
+    int front, rear;    // 头尾位置
+};
+
+
+///////////////////////////////////////
+/// 基本操作
+
+///
+/// 初始化空队列
+///
+template<typename T, int M>
+void InitQueue(SqQueue<T,M>& Q)
+{
+    Q.front = Q.rear = 0;
+}
+
+///
+/// 队列判空
+///
+template<typename T, int M>
+bool QueueEmpty(const SqQueue<T,M>& Q)
+{
+    return Q.front == Q.rear;
+}
+
+///
+/// 入队列
+///
+template<typename T, int MAXSIZE>
+void EnQueue(SqQueue<T,MAXSIZE>& Q, T e)
+{
+    // 若队列满，则上溢
+    if((Q.rear+1) % MAXSIZE == Q.front)
+        throw overflow_error("Queue full");
+    // 在队尾插入元素
+    Q.elem[Q.rear] = e;
+    Q.rear = (Q.rear+1) % MAXSIZE;
+}
+
+///
+/// 出队列
+///
+template<typename T, int MAXSIZE>
+void DeQueue(SqQueue<T,MAXSIZE>& Q, T& e)
+{
+    // 若队列空，则出错
+    if(Q.front == Q.rear)
+        throw underflow_error("Queue empty");
+    // 返回并删除队头元素
+    e = Q.elem[Q.front];
+    Q.front = (Q.front+1) % MAXSIZE;
+}
+
+///
+/// 出队列
+///
+template<typename T, int MAXSIZE>
+T DeQueue(SqQueue<T,MAXSIZE>& Q)
+{
+    T e;
+    DeQueue(Q,e);
+    return e;
+}
+
+
+///
+/// 遍历队列中的元素
+///
+template<typename T, int MAXSIZE, typename F>
+void QueueTraverse(const SqQueue<T,MAXSIZE>& Q, F visit)
+{
+    for(int i=Q.front; i<Q.rear; i=(i+1)%MAXSIZE) {
+        visit(Q.elem[i]);
+    }
+}
---
+++ src/ch03/sqqueue_test.cpp
@@ -0,0 +1,42 @@
+///////////////////////////////////////
+/// file: sqqueue_test.cpp
+/// 循环队列测试程序
+///////////////////////////////////////
+
+#include <iostream>
+#include "sqqueue.h"
+using namespace std;
+
+///
+/// 循环队列测试程序
+///
+int main()
+{
+    // 打印数据功能
+    auto print = [](int x) { cout << x << " "; };
+
+    // 创建队列并初始化
+    SqQueue<int,10> Q;
+    InitQueue(Q);
+    QueueTraverse(Q, print);
+    cout << "\nQueueEmpty = " << QueueEmpty(Q) << endl;
+    
+    // 入队列
+    EnQueue(Q, 3);
+    EnQueue(Q, 1);
+    EnQueue(Q, 4);
+    EnQueue(Q, 1);
+    EnQueue(Q, 5);
+    EnQueue(Q, 9);
+    QueueTraverse(Q, print);
+    cout << "\nQueueEmpty = " << QueueEmpty(Q) << endl;
+
+    // 出队列
+    while(!QueueEmpty(Q)) {
+        auto e = DeQueue(Q);
+        cout << "DeQueue = " << e << endl;
+    }
+    cout << "\nQueueEmpty = " << QueueEmpty(Q) << endl;    
+    
+    return 0;
+}
