a5c7389d2f91dde523928c404bbba46f53150c69
Bo Zhuang <sdzhuangbo@126.com>
2017-09-20 03:55:35 +0800

    Fix: conversion zero

--- src/ch03/conversion.cpp
+++ src/ch03/conversion.cpp
@@ -22,10 +22,10 @@ void conversion()
     // 转换为八进制数
     SqStack<int> s;
     InitStack(s); // 初始化为空栈
-    while(n) {
+    do {
         Push(s, n%8);
         n = n / 8;
-    }
+    } while(n);
 
     // 输出八进制数
     while(!StackEmpty(s)) {
