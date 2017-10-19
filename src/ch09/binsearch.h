8ffccede900e01a28258211fc40ddc5374e148e7
Bo Zhuang <sdzhuangbo@126.com>
2017-10-19 21:12:18 +0800

    Update binary search

--- src/ch09/binsearch.h
+++ src/ch09/binsearch.h
@@ -10,7 +10,7 @@
 /// 若找到，返回元素在数组中的下标，否则返回 -1。
 ///
 template <typename E>
-int BinarySearch(E a[], int n, const E &x)
+int BinarySearch(E a[], int n, E x)
 {
     int low, mid, high;
 
@@ -19,13 +19,31 @@ int BinarySearch(E a[], int n, const E &x)
     while (low <= high)
     {
         mid = (low + high) / 2;
+        if (x == a[mid])
+            return mid;
         if (x < a[mid])
             high = mid - 1;
-        else if (x > a[mid])
-            low = mid + 1;
         else
-            return mid;
+            low = mid + 1;
     }
 
     return -1; // not found
+}
+
+///
+/// 折半查找
+///
+template <typename E>
+int BinarySearch(E a[], int low, int high, E x)
+{
+    if (low > high)
+        return -1; // not found
+
+    int mid = (low + high) / 2;
+    if (x == a[mid])
+        return mid;
+    else if (x < a[mid])
+        return BinarySearch(a, low, mid - 1, x);
+    else
+        return BinarySearch(a, mid + 1, high, x);
 }
