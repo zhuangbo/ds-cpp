9b47f7f1755a111acef8c97e6d946e55cb2a0b1c
Bo Zhuang <sdzhuangbo@126.com>
2017-10-26 15:30:29 +0800

    Fix selection sort

--- src/ch10/selectionsort.h
+++ src/ch10/selectionsort.h
@@ -11,14 +11,14 @@
 template <typename T>
 void SelectionSort(T a[], int n)
 {
-    for (int i = 1; i < n; i++) {
-        // 从 a[i..n-1] 中选出最小的 a[k] 与 a[i-1] 交换
+    for (int i = 0; i < n - 1; i++) {
+        // 从 a[i..n-1] 中选出最小的 a[k] 与 a[i] 交换
         int k = i;
         int j = i + 1;
         for (; j < n; j++)
             if (a[j] < a[k])
                 k = j;
         if (j != k)
-            swap(a[k], a[i - 1]);
+            swap(a[k], a[i]);
     }
 }
